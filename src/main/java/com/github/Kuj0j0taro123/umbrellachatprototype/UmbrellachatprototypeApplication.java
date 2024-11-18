750120dada187678cf3a04235f9222916ce8a983
OV3RDR1VE <coredumpedsegmentationerror@protonmail.com>
2024-11-18 15:27:33 +0200

    first commit

---
+++ src/main/java/com/github/Kuj0j0taro123/umbrellachatprototype/UmbrellachatprototypeApplication.java
@@ -0,0 +1,13 @@
+package com.github.Kuj0j0taro123.umbrellachatprototype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbrellachatprototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbrellachatprototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Kuj0j0taro123/umbrellachatprototype/UmbrellachatprototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Kuj0j0taro123.umbrellachatprototype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmbrellachatprototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
